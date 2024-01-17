909993bc836752c47f9b306652e88ad58a5c6093
HoangLong08082001 <hahoanglong08082001@gmail.com>
2024-01-17 18:46:21 +0700

    17/1/2024

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,12 +1,12 @@
-const { createPool } = require("mysql");
-const dotenv = require("dotenv").config();
+const { createPool }=require('mysql');
+
 const pool = createPool({
-  port: process.env.DB_PORT,
-  host: process.env.POST,
-  user: process.env.USER,
-  password: process.env.PASS ,
-  database: process.env.MYSQL_DB || "travel-ui",
-  connectionLimit: 10,
+    port: process.env.DB_PORT,
+    host:process.env.POST,
+    user:process.env.USER,
+    password:process.env.PASS,
+    database:process.env.MYSQL_DB,
+    connectionLimit:10
 });
 
 module.exports = pool;
