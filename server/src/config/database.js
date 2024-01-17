ebca7ac78649195d5500efb0ec4c4d208b0f365d
HoangLong08082001 <hahoanglong08082001@gmail.com>
2024-01-17 18:58:08 +0700

    update add 17/1/2024

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,12 +1,13 @@
-const { createPool }=require('mysql');
-
+const { createPool } = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config();
 const pool = createPool({
-    port: process.env.DB_PORT,
-    host:process.env.POST,
-    user:process.env.USER,
-    password:process.env.PASS,
-    database:process.env.MYSQL_DB,
-    connectionLimit:10
+  port: process.env.DB_PORT,
+  host: process.env.POST,
+  user: process.env.USER,
+  password: process.env.PASS,
+  database: process.env.MYSQL_DB,
+  connectionLimit: 10,
 });
 
 module.exports = pool;
